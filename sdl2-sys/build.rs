fee19581769adb8185fa81c3ced3d5fe1551d9b5
Adam Gausmann <agausmann@fastmail.com>
2018-08-15 05:10:09 -0500

    Enforce the use of an existing installation - less bloat

--- sdl2-sys/build.rs
+++ sdl2-sys/build.rs
@@ -29,9 +29,6 @@ const LASTEST_SDL2_VERSION: &str = "2.0.8";
 // The latest stable (tagged) port for the emscripten target
 const LATEST_EMSCRIPTEN_SDL2_TAG: &str = "version_13";
 
-// Version of emscripten to use.
-const LATEST_EMSCRIPTEN_VERSION: &str = "1.38.11";
-
 #[cfg(feature = "bindgen")]
 macro_rules! add_msvc_includes_to_bindings {
     ($bindings:expr) => {
@@ -131,34 +128,6 @@ fn download_sdl2(target_os: &str) -> PathBuf {
     sdl2_build_path
 }
 
-#[cfg(feature = "bundled")]
-fn download_emscripten() -> PathBuf {
-    // Use the already-activated toolchain if it exists:
-    if let Ok(path) = env::var("EMSCRIPTEN") {
-        return PathBuf::from(path);
-    }
-
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let archive_path = out_dir.join(format!("emscripten-{}.tar.gz", LATEST_EMSCRIPTEN_VERSION));
-        
-    if !archive_path.exists() {
-        let archive_url = format!(
-            "https://github.com/kripken/emscripten/archive/{}.tar.gz",
-            LATEST_EMSCRIPTEN_VERSION,
-        );
-        let archive_file = fs::File::create(&archive_path).unwrap();
-        download_to(&archive_url, &archive_file);
-    }
-
-    let reader = flate2::read::GzDecoder::new(
-        fs::File::open(&archive_path).unwrap()
-    ).unwrap();
-    let mut archive = tar::Archive::new(reader);
-    archive.unpack(&out_dir).unwrap();
-
-    out_dir.join(format!("emscripten-{}", LATEST_EMSCRIPTEN_VERSION))
-}
-
 // compile a shared or static lib depending on the feature 
 #[cfg(feature = "bundled")]
 fn compile_sdl2(sdl2_build_path: &Path, target_os: &str) -> PathBuf {
@@ -168,7 +137,8 @@ fn compile_sdl2(sdl2_build_path: &Path, target_os: &str) -> PathBuf {
     if target_os == "windows-gnu" {
         cfg.define("VIDEO_OPENGLES", "OFF");
     } else if target_os == "emscripten" {
-        let emscripten_toolchain_file = download_emscripten().join("cmake")
+        let emscripten_home = PathBuf::from(env::var("EMSCRIPTEN").unwrap());
+        let emscripten_toolchain_file = emscripten_home.join("cmake")
             .join("Modules").join("Platform").join("Emscripten.cmake");
 
         cfg.define("CMAKE_TOOLCHAIN_FILE", emscripten_toolchain_file);
